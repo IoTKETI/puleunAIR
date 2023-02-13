524aeae89babad85a166bb122990dbd133ac1d34
dnjstjr93 <dnjstjr93@gmail.com>
2023-02-13 11:51:19 +0900

    Initial commit

--- terminal_app.js
+++ terminal_app.js
@@ -4,6 +4,7 @@
 
 const mqtt = require('mqtt');
 const {nanoid} = require('nanoid');
+const fs = require("fs");
 const term = require('terminal-kit').terminal;
 
 let local_mqtt_client = null;
@@ -20,7 +21,7 @@ let humidity = 0.0;
 
 local_mqtt_connect('127.0.0.1');
 
-let control_items = ['Control_1', 'Control_2', 'Control_3', 'Control_4', 'Control_5'];
+const control_items = ['Quit', 'Control_1', 'Control_2', 'Control_3', 'Control_4', 'Control_5', 'All'];
 
 function local_mqtt_connect(serverip) {
     if (local_mqtt_client === null) {
@@ -74,37 +75,9 @@ function local_mqtt_connect(serverip) {
     }
 }
 
-function set_control(point, val) {
-    if (point === 1) {
-        if (local_mqtt_client !== null) {
-            local_mqtt_client.publish(set_Control_1_topic, val);
-        }
-    } else if (point === 2) {
-        if (local_mqtt_client !== null) {
-            local_mqtt_client.publish(set_Control_2_topic, val);
-        }
-    } else if (point === 3) {
-        if (local_mqtt_client !== null) {
-            local_mqtt_client.publish(set_Control_3_topic, val);
-        }
-    } else if (point === 4) {
-        if (local_mqtt_client !== null) {
-            local_mqtt_client.publish(set_Control_4_topic, val);
-        }
-    } else if (point === 5) {
-        if (local_mqtt_client !== null) {
-            local_mqtt_client.publish(set_Control_5_topic, val);
-        }
-    } else {
-        console.log('invalid point');
-    }
-}
-
 let startMenuIndex = 0;
 let startMenuDroneSelected = 'none';
-let cur_goto_position = 'none';
-let cur_mode_selected = 'none';
-let cur_drone_list_selected = [];
+let cur_control_list_selected = [];
 let cur_command_items = [];
 let command_items = ['Back', 'ON', 'OFF'];
 let startmenu_list = control_items;
@@ -116,9 +89,6 @@ function startMenu() {
         selectedStyle: term.dim.yellow.bgGray,
         selectedIndex: startMenuIndex
     };
-
-    startmenu_list.unshift('Quit');
-    startmenu_list.push('All');
 
     term.singleLineMenu(startmenu_list, _options, function (error, response) {
         term('\n').eraseLineAfter.moveTo.green(1, 2,
@@ -137,20 +107,106 @@ function startMenu() {
         cur_command_items = [].concat(command_items);
 
         if (startMenuDroneSelected === 'All') {
-            cur_drone_list_selected = [].concat(control_items);
-
-            cur_command_items.splice(cur_command_items.indexOf('Follow'), 1);
-            //cur_command_items.splice(cur_command_items.indexOf('Real_Control'), 1);
+            cur_control_list_selected = [].concat(control_items);
         } else if (startMenuDroneSelected === 'Quit') {
             process.exit();
         } else {
-            cur_drone_list_selected = [].concat(control_items[startMenuIndex - 1]);
+            cur_control_list_selected = [].concat(control_items[startMenuIndex - 1]);
         }
-        //
-        // term('\n').eraseDisplayBelow();
-        //
-        // allMenu();
+
+        term('\n').eraseDisplayBelow();
+
+        allMenu();
     });
+}
+
+let placeFlag = '';
+let printFlag = '';
+let curAllMenuIndex = 0;
+const back_menu_delay = 100;
+
+function allMenu() {
+    placeFlag = 'allMenu';
+    printFlag = 'enable';
+
+    var _options = {
+        y: 1,	// the menu will be on the top of the terminal
+        style: term.inverse,
+        selectedStyle: term.dim.blue.bgGreen,
+        selectedIndex: curAllMenuIndex
+    };
+
+    term.singleLineMenu(cur_command_items, _options, function (error, response) {
+        term('\n').eraseLineAfter.moveTo.green(1, 2,
+            "#%s selected: %s (%s,%s)\n",
+            response.selectedIndex,
+            response.selectedText,
+            response.x,
+            response.y
+        );
+
+        curAllMenuIndex = response.selectedIndex;
+
+        if (startMenuDroneSelected === 'All') {
+            cur_control_list_selected = [].concat(control_items);
+        } else {
+            cur_control_list_selected = [].concat(control_items[startMenuIndex - 1]);
+        }
+
+        if (response.selectedText === 'Back') {
+            setTimeout(startMenu, back_menu_delay);
+        } else if (response.selectedText === 'ON') {
+            allOnMenu();
+        } else if (response.selectedText === 'OFF') {
+            allOffMenu();
+        } else {
+            setTimeout(startMenu, back_menu_delay);
+        }
+    });
+}
+
+let column_count = 0;
+
+function allOnMenu() {
+    term.eraseDisplayBelow();
+    term.moveTo.red(1, 3, '');
+
+    for (let idx in cur_control_list_selected) {
+        if (cur_control_list_selected.hasOwnProperty(idx)) {
+            let control_selected = cur_control_list_selected[idx];
+
+            if (control_selected.includes('Control')) {
+                if (local_mqtt_client !== null) {
+                    local_mqtt_client.publish('/puleunair/' + control_selected + '/set', '1', () => {
+                        console.log('Send ON command to ' + control_selected);
+                    });
+                }
+            }
+        }
+    }
+
+    setTimeout(allMenu, back_menu_delay * (cur_control_list_selected.length + 1));
+}
+
+function allOffMenu() {
+    term.eraseDisplayBelow();
+    term.moveTo.red(1, 3, '');
+
+    for (let idx in cur_control_list_selected) {
+        if (cur_control_list_selected.hasOwnProperty(idx)) {
+            let control_selected = cur_control_list_selected[idx];
+
+            if (control_selected.includes('Control')) {
+                if (local_mqtt_client !== null) {
+                    local_mqtt_client.publish('/puleunair/' + control_selected + '/set', '0', () => {
+                        console.log('Send OFF command to ' + control_selected);
+                    });
+                }
+            }
+        }
+    }
+
+    setTimeout(allMenu, back_menu_delay * (cur_control_list_selected.length + 1));
 }
 
 setTimeout(startMenu, 1000);
