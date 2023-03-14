9454d62d3bf46f413e284bb417f9d8da05c639b6
dnjstjr93 <dnjstjr93@gmail.com>
2023-03-14 18:31:26 +0900

    - 수온, 온습도 센서 변경

--- read_TempHumi.py
+++ read_TempHumi.py
@@ -1,13 +1,86 @@
 from SHT31 import SHT31
 import time
+import paho.mqtt.client as mqtt
+import threading
+from datetime import datetime
+
+pureunHost = '121.137.228.240'
+
+mqtt_client = None
+pub_temphumi_topic = "/puleunair/temphumi"
+
 sht31 = SHT31()
 
-while True:
-        sht31.write_command()
-        time.sleep(0.3)
-        result = sht31.read_data()
-        print ("Relative Humidity : %.2f %%"%(result['h']))
-        print ("Temperature in Celsius : %.2f C"%(result['c']))
-        print ("Temperature in Fahrenheit : %.2f F"%(result['f']))
-        print (" ************************************* ")
-        time.sleep(1)
+
+def read_temphumi():
+    sht31.write_command()
+    time.sleep(0.3)
+    result = sht31.read_data()
+    if mqtt_client is not None:
+        mqtt_client.publish(pub_temphumi_topic, result['c'] + ',' + result['h'])
+    else:
+        mqtt_client.reconnect()
+
+    print('\n', datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'))
+    print("Relative Humidity : %.2f %%" % (result['h']))
+    print("Temperature in Celsius : %.2f C" % (result['c']))
+
+    threading.Timer(3, read_temphumi).start()
+
+
+def on_connect(client, userdata, flags, rc):
+    # 0: Connection successful
+    # 1: Connection refused - incorrect protocol version
+    # 2: Connection refused - invalid client identifier
+    # 3: Connection refused - server unavailable
+    # 4: Connection refused - bad username or password
+    # 5: Connection refused - not authorised
+    # 6-255: Currently unused.
+    global mqtt_client
+    global pureunHost
+
+    if rc is 0:
+        print('[mqtt_client_connect] connect to ' + pureunHost)
+    elif rc is 1:
+        print("incorrect protocol version")
+        mqtt_client.reconnect()
+    elif rc is 2:
+        print("invalid client identifier")
+        mqtt_client.reconnect()
+    elif rc is 3:
+        print("server unavailable")
+        mqtt_client.reconnect()
+    elif rc is 4:
+        print("bad username or password")
+        mqtt_client.reconnect()
+    elif rc is 5:
+        print("not authorised")
+        mqtt_client.reconnect()
+    else:
+        print("Currently unused.")
+        mqtt_client.reconnect()
+
+
+def on_disconnect(client, userdata, flags, rc=0):
+    print(str(rc))
+
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_message(client, userdata, _msg):
+    print("Received " + _msg.payload.decode('utf-8') + " From " + _msg.topic)
+
+
+if __name__ == "__main__":
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_disconnect = on_disconnect
+    mqtt_client.on_subscribe = on_subscribe
+    mqtt_client.on_message = on_message
+    mqtt_client.connect("121.137.228.240", 1883)
+
+    mqtt_client.loop_start()
+
+    read_temphumi()
--- read_waterTemp.py
+++ read_waterTemp.py
@@ -1,34 +1,85 @@
-# -*- coding: utf-8 -*-
-'''!
-  @file read_temp.py
-  @brief Get probe temperature
-  @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
-  @license     The MIT License (MIT)
-  @author      PengKaixing(kaixing.peng@dfrobot.com)
-  @version  V1.0.0
-  @date  2022-04-01
-  @url https://github.com/DFRobot/DFRobot_MAX31855
-'''
-import sys
-import os
-import time
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 from DFRobot_MAX31855 import *
+import paho.mqtt.client as mqtt
+import threading
+from datetime import datetime
+
+pureunHost = '121.137.228.240'
+
+mqtt_client = None
+pub_hotwater_topic = "/puleunair/hotwater"
 
 I2C_1 = 0x01
 I2C_ADDRESS = 0x10
-# Create MAX31855 object
+
 max31855 = DFRobot_MAX31855(I2C_1, I2C_ADDRESS)
 
 
-def loop():
-    while True:
-        # Read celsius
-        temp = max31855.read_celsius()
-        print("Temperature:%s C" % temp)
-        time.sleep(1)
+def read_hotwater():
+    temp = max31855.read_celsius()
+    if mqtt_client is not None:
+        mqtt_client.publish(pub_hotwater_topic, temp)
+    else:
+        mqtt_client.reconnect()
+
+    print('\n', datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'))
+    print("Water Temperature:%s C" % temp)
+
+    threading.Timer(3, read_hotwater).start()
+
+
+def on_connect(client, userdata, flags, rc):
+    # 0: Connection successful
+    # 1: Connection refused - incorrect protocol version
+    # 2: Connection refused - invalid client identifier
+    # 3: Connection refused - server unavailable
+    # 4: Connection refused - bad username or password
+    # 5: Connection refused - not authorised
+    # 6-255: Currently unused.
+    global mqtt_client
+    global pureunHost
+
+    if rc is 0:
+        print('[mqtt_client_connect] connect to ' + pureunHost)
+    elif rc is 1:
+        print("incorrect protocol version")
+        mqtt_client.reconnect()
+    elif rc is 2:
+        print("invalid client identifier")
+        mqtt_client.reconnect()
+    elif rc is 3:
+        print("server unavailable")
+        mqtt_client.reconnect()
+    elif rc is 4:
+        print("bad username or password")
+        mqtt_client.reconnect()
+    elif rc is 5:
+        print("not authorised")
+        mqtt_client.reconnect()
+    else:
+        print("Currently unused.")
+        mqtt_client.reconnect()
+
+
+def on_disconnect(client, userdata, flags, rc=0):
+    print(str(rc))
+
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_message(client, userdata, _msg):
+    print("Received " + _msg.payload.decode('utf-8') + " From " + _msg.topic)
 
 
 if __name__ == "__main__":
-    loop()
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_disconnect = on_disconnect
+    mqtt_client.on_subscribe = on_subscribe
+    mqtt_client.on_message = on_message
+    mqtt_client.connect("121.137.228.240", 1883)
+
+    mqtt_client.loop_start()
+
+    read_hotwater()
